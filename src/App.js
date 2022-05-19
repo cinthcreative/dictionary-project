9687d7a3699b49b4bbce6dc76983ebf2e52fd93f
cinthcreative <47701540+cinthcreative@users.noreply.github.com>
2022-05-18 17:49:47 -0700

    Created search form.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./ck-butterfly_Shape logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Cinthia Koutianov</small>
+        </footer>
+      </div>
     </div>
   );
 }
